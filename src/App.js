825cda98cc6b46055224302880fc435178fd2e2b
shashanknenar <shashanknenar@gmail.com>
2020-11-17 21:31:48 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpense } from './components/IncomeExpense'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance />
+        <IncomeExpense/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
