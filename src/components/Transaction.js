1150e5c0faeeb6df4578bcd326d2961ca3eef39c
shashanknenar <shashanknenar@gmail.com>
2020-11-17 22:28:19 +0530

    balance

---
+++ src/components/Transaction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Transaction = ({ transaction}) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    return (
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text} <span>{sign}₹{Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+        </li>
+    )
+}
